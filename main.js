9dc93fc2fb881fdf0f9cde07ce16cf145020b007
hannanel100 <hannanel100@gmail.com>
2022-12-05 22:00:19 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const ratings = document.getElementsByClassName("card__body__rating__item");
+const submitButton = document.getElementById("submit");
+const initialCard = document.getElementById("rating__form");
+const finalCard = document.getElementById("rating__submitted");
+const rating = document.getElementById("rating");
+// get html data-value attribute
+let selected = "";
+const handleRatingClick = (e) => {
+  selected = e.target.getAttribute("data-value");
+  console.log(e.target.getAttribute("data-value"));
+  //   add class selected to clicked element
+  e.target.classList.add("selected");
+  //   change inner text of rating element
+  rating.innerText = selected;
+  //   remove class selected from other elements
+  for (let i = 0; i < ratings.length; i++) {
+    if (ratings[i].getAttribute("data-value") !== selected) {
+      ratings[i].classList.remove("selected");
+    }
+  }
+  //   change submit button from disabled
+  submitButton.disabled = false;
+};
+const handleSubmit = () => {
+  console.log(selected);
+  //   add class not-visible to initial card
+  initialCard.classList.add("not-visible");
+  //   remove class not-visible from final card
+  finalCard.classList.remove("not-visible");
+};
+for (let i = 0; i < ratings.length; i++) {
+  ratings[i].addEventListener("click", handleRatingClick);
+}
+
+submitButton.addEventListener("click", handleSubmit);
+console.log(selected);
